fbf505ebad3532b673bcbb941a38506b264af496
z1441275629 <1441275629@qq.com>
2018-05-11 10:20:42 +0800

    第一次发布测试的自定义模板

---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <b>hello，<i>from 赵德升</i></b>
+	<p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'nice to meet you'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
